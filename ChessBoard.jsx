0507b4ca6cd64f445102321c18e0d834c314c029
Julian Bolanos <julian.camilob24@gmail.com>
2022-03-01 11:50:58 -0500

    ChessBoard class added

---
+++ ChessBoard.jsx
@@ -0,0 +1,11 @@
+class ChessBoard extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello chess
+            </div>
+        );
+    }
+}
+
+ReactDOM.render();
